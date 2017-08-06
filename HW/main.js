22b06949f6e67cc05197bec38e70ce1aee25707a
יהודית אילסון <JudithI@kkl.org.il>
2017-08-06 16:47:23 +0300

    changes...

---
+++ HW/main.js
@@ -0,0 +1,24 @@
+//Exercise 1
+function calculateTriangle(sideOne, sideTwo, sideThree) {
+    var sidesLengthAverage;
+    var trianleCircumference;
+    var trianleArea;
+    
+    
+    
+    
+    
+    return "";
+}
+
+
+
+//Exercise 2
+
+
+
+
+
+
+
+//Execrcise 3
